031ab801520cb7e866382e88b8cfe77729ecc643
thtguytho <imre@thtguytho.nl>
2021-02-08 02:29:35 +0100

    Replaced createReadStream with filePath

--- src/modules/voice.js
+++ src/modules/voice.js
@@ -85,8 +85,7 @@ const Voice = {
         }
 
         const connection = await channel.join();
-        console.log({ now_playing: filePath });
-        const dispatcher = await connection.play(fs.createReadStream(filePath));
+        const dispatcher = await connection.play(filePath);
 
         const fileRemovalFallback = setTimeout(() => {
           if (type === 'remove') fs.unlink(filePath);
